1f7de90a5c344d181f5b91dd133afcf386ea8e2b
Taufik Hidayah <taufikhidayah980@gmail.com>
2025-08-11 12:13:37 +0800

    added code for submit-ticket js function

--- api/src/submitTicket/index.js
+++ api/src/submitTicket/index.js
@@ -1,10 +1,10 @@
-const { app } = require('@azure/functions');
-const { CosmosClient } = require('@azure/cosmos');
-const { v4: uuidv4 } = require('uuid');
+const { app } = require("@azure/functions");
+const { CosmosClient } = require("@azure/cosmos");
+const { v4: uuidv4 } = require("uuid");
 
-app.http('submitTicket', {
-  methods: ['POST'],
-  authLevel: 'anonymous',
+app.http("submitTicket", {
+  methods: ["POST"],
+  authLevel: "anonymous",
   handler: async (request, context) => {
     const endpoint = process.env.COSMOS_ENDPOINT;
     const key = process.env.COSMOS_KEY;
@@ -22,14 +22,14 @@ app.http('submitTicket', {
         status: 201,
         jsonBody: {
           message: "Ticket submitted successfully!",
-          ticket_id: ticket.id
-        }
+          ticket_id: ticket.id,
+        },
       };
     } catch (err) {
       return {
         status: 500,
-        body: `Error: ${err.message}`
+        body: `Error: ${err.message}`,
       };
     }
-  }
+  },
 });
