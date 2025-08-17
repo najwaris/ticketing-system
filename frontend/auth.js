f4bf80c256fbd0a14e0e4db47c6ab767c698d139
Taufik Hidayah <taufikhidayah980@gmail.com>
2025-08-18 00:10:40 +0800

    feat: added security (login & logout) and route protection

---
+++ frontend/auth.js
@@ -0,0 +1,48 @@
+const msalConfig = {
+  auth: {
+    clientId: "506c4789-8d9b-402d-9507-37561ea5c9d7",
+    authority: "https://login.microsoftonline.com/common",
+    redirectUri: "http://localhost:5501/frontend/home.html",
+  },
+};
+
+const loginRequest = {
+  scopes: ["User.Read"],
+};
+
+const msalInstance = new msal.PublicClientApplication(msalConfig);
+msalInstance.handleRedirectPromise().then((response) => {
+  if (response) {
+    msalInstance.setActiveAccount(response.account);
+    localStorage.setItem("isAuthenticated", "true");
+    window.location.href = "home.html"; // send user to homepage after login
+  } else {
+    // If we're already in home.html but not logged in, force back to index
+    if (
+      window.location.pathname.endsWith("home.html") &&
+      !localStorage.getItem("isAuthenticated")
+    ) {
+      window.location.href = window.location.origin + "/frontend/index.html";
+    }
+  }
+});
+const handleLogin = document.getElementById("loginBtn");
+if (handleLogin) {
+  handleLogin.onclick = async () => {
+    msalInstance.loginRedirect({
+      ...loginRequest,
+      prompt: "select_account",
+    });
+  };
+}
+
+const handleLogout = document.getElementById("logoutBtn");
+if (handleLogout) {
+  handleLogout.onclick = async () => {
+    localStorage.removeItem("isAuthenticated");
+    await msalInstance.logoutRedirect({
+      postLogoutRedirectUri: window.location.origin + "/frontend/index.html",
+      account: msalInstance.getActiveAccount(),
+    });
+  };
+}
